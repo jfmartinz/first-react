55c3339ad69e47143747785ccb506c71da26552f
Joseph Martin <se.josephmartin@gmail.com>
2025-07-31 15:51:52 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
 // import Profile from './Profile';
 
 export default function Gallery() {
-  return (
-    <div>
-      <h1>My babyy so pretty!! 🥰😊</h1>
-      <img
-        src='/em.jpeg'
-        alt='Hedy Lamarr'
-        className='photo'
-      />
-      <h1>Ilovemybabyyysomuchh!! 🥰😊</h1>
-    </div>
-  );
+  return <div></div>;
 }
