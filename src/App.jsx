349e3c55d005a79921476ffe9ce6ebf5ff7e155f
Joseph Martin <se.josephmartin@gmail.com>
2025-08-05 17:33:26 +0800

    add props exercise #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import UserCard from './UserCard.jsx';
+import './App.css';
+
+function AlertBox({ children, type }) {
+  const bgColor = {
+    success: 'green',
+    error: 'red',
+    warning: 'yellow',
+  };
+
+  return (
+    <div className='layout'>
+      <h1>{type}</h1>
+      <div style={{ backgroundColor: bgColor[type] }}>{children}</div>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <>
-      <UserCard
-        name='John Doe'
-        age={30}
-        isOnline={true}
-      />
-      <UserCard
-        name='Jane Smith'
-        age={25}
-        isOnline={true}
-      />
+      <AlertBox type='success'>
+        <p>This is a success message.</p>
+      </AlertBox>
+      <AlertBox type='error'>
+        <p>This is an error message.</p>
+      </AlertBox>
+      <AlertBox type='warning'>
+        <p>This is warning message.</p>
+      </AlertBox>
     </>
   );
 }
